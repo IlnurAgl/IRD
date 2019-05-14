5017267fb074f30c1c5fc6c98893d47e123462a0
IlnurAgl <45607322+IlnurAgl@users.noreply.github.com>
2019-05-14 14:42:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, TaskForm
 from flask_login import UserMixin
 from datetime import datetime
 from flask_bcrypt import Bcrypt
@@ -29,20 +29,23 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 # Создание модели отправки
-class Post(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
+    date_done = db.Column(db.Text, nullable=False)
+    user_name = db.Column(db.Text, nullable=False)
+    executor = db.Column(db.Text)
+    priority = db.Column(db.Text)
+    category = db.Column(db.Text)
+    stage = db.Column(db.Text)
+    done = db.Column(db.Text)
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
 
@@ -55,13 +58,15 @@ db.create_all()
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    if 'username' not in session:
+        return redirect('register')
+    return render_template('home.html', username=session['username'], tasks=Task.query.filter_by(user_name=session['username']))
 
 @app.route('/')
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if 'username' in session:
-        return redirect(url_for('home'))
+        return redirect('home', username=session['username'])
     # Форма регистрации
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -85,7 +90,7 @@ def register():
 def login():
     form = LoginForm()
     if 'username' in session:
-        return redirect(url_for('home'))
+        return redirect('home')
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if not user:
@@ -100,6 +105,20 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+@app.route('/add-task', methods=['GET', 'POST'])
+def add_task():
+    form = TaskForm()
+    if 'username' not in session:
+        return redirect('login')
+    if form.validate_on_submit():
+        task = Task(title=form.title.data, content=form.content.data, user_name=session['username'], executor=form.executor.data, priority=form.priority.data, category=form.category.data, stage=form.stage.data, done=form.done.data, date_done=str(form.date_done.data))
+        db.session.add(task)
+        db.session.commit()
+        return redirect('home')
+    return render_template('add-task.html', title='Add task', form=form, username=session['username'])
+
+
+# Выход из аккаунта
 @app.route('/logout')
 def logout():
     session.pop('username', 0)
